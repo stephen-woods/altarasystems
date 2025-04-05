363e1c7a8e360c9b13364990bbefaef7b74627b7
Stephen Woods <stephen_woods@cable.comcast.com>
2025-04-05 18:52:47 -0400

    Initial commit

---
+++ javascripts/fonts.js
@@ -0,0 +1,7 @@
+//CUFON
+Cufon.replace('a.entry-title', { hover: true });
+Cufon('a.entry-title', { color: '#494b4b', hover: {	color: '#000' }	});
+Cufon('a.mini-title', { color: '#363636', hover: {	color: '#000' }	});
+
+Cufon.replace('.product-description h2, span.subtitle');
+Cufon.replace('h1, h2, h3, #sidebar h4, .contact-nav');
